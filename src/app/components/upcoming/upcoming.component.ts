d2d3baa5f1913e0e863956c928215ea6cedc860c
suren <37803009+akhill12@users.noreply.github.com>
2023-10-23 17:54:24 -0400

    home-updated

--- src/app/components/upcoming/upcoming.component.ts
+++ src/app/components/upcoming/upcoming.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+import { ScheduledmatchesService } from 'src/app/services/scheduledmatches.service';
 
 @Component({
   selector: 'app-upcoming',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatIconModule],
   templateUrl: './upcoming.component.html',
   styleUrls: ['./upcoming.component.scss']
 })
-export class UpcomingComponent {
+export class UpcomingComponent implements OnInit{
+
+  upcomingMatchService = inject(ScheduledmatchesService);
+
+  scheduled: any = null;
+
+  ngOnInit(): void {
+   this.scheduled = this.upcomingMatchService.getScheduledMatches("1");
+
+  }
 
 }
